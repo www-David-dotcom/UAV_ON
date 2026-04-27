964bb98dfa7908ea12b928f8ff7345f4bd5795b4
sbx24 <sbx24@mails.tsinghua.edu.cn>
2026-04-27 17:13:05 +0000

    added GPU binding on eval-cliph.sh

--- scripts/eval_cliph.sh
+++ scripts/eval_cliph.sh
@@ -2,13 +2,14 @@ cd "$(dirname "$0")/.."
 root_dir=.
 echo $PWD
 
-CUDA_VISIBLE_DEVICES=0 python -u $root_dir/src/eval_cliph.py \
+GPU=${GPU:-0}
+
+CUDA_VISIBLE_DEVICES=$GPU python -u $root_dir/src/eval_cliph.py \
     --maxActions 150 \
     --eval_save_path $root_dir/CLIP_logs/scene \
-    --dataset_path ../DATASET/val_all.json  \
+    --dataset_path ../DATASET/val_all.json \
     --is_fixed  true\
-    --gpu_id 0 \
+    --gpu_id $GPU \
     --batchSize 1 \
     --simulator_tool_port 30000
-
 
